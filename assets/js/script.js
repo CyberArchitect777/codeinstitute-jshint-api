875973e7711c9749cf8a48c6cd30f09f0351addd
“CyberArchitect777” <169656116+CyberArchitect777@users.noreply.github.com>
2024-06-26 16:04:35 +0000

    Implemented first part of the check API key function

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+const API_KEY = "Ps5KRDBPB7WWGPKBhc43eOvF_8M";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+    const response = await fetch(queryString);
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data);
+    }
+}
